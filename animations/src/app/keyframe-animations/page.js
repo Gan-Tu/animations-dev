062efc4c4f2ad81c32de4e501e4f8111afa65a8a
Gan Tu <dev@tugan.io>
2025-01-18 17:13:32 -0500

    add orbit animation

--- animations/src/app/keyframe-animations/page.js
+++ animations/src/app/keyframe-animations/page.js
@@ -6,6 +6,9 @@ export default function KeyframeAnimations() {
       <li className="underline">
         <Link href="/keyframe-animations/text-reveal">Text Reveal</Link>
       </li>
+      <li className="underline">
+        <Link href="/keyframe-animations/orbit-animation">Orbit Animation</Link>
+      </li>
     </ul>
   )
 }
